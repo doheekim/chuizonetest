2b78c6efe05812965fb2972bcfdc65a35f3e3ccd
doheekim <dohee.4678@gmail.com>
2014-08-21 19:11:52 +0900

    model update

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class Academy(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	academy_name = db.Column(db.String(255))
+	teacher_name = db.Column(db.String(255))
+	academy_introduce = db.Column(db.Text())
+	teacher_introduce = db.Column(db.Text())
+	curriculum_introduce = db.Column(db.Text())
+	academy_address = db.Column(db.String(255))
+	welcome_line = db.Column(db.String(255))
+	phone_number = db.Column(db.String(255))
+	class_time = db.Column(db.Text())
+	class_fee = db.Column(db.String(255))
+	homepage = db.Column(db.String(255))
