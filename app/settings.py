da375757706a18ca8e719c71da6157838664a91b
doheekim <dohee.4678@gmail.com>
2014-08-21 17:33:30 +0900

    no message

---
+++ app/settings.py
@@ -0,0 +1,11 @@
+class Config(object):
+	SECRET_KEY = "aaaaaaa"
+	debug = False
+
+
+class Production(Config):
+	debug = True
+	CSRF_ENABLED = False
+	ADMIN = "chuizone.com@gmail.com"
+	SQLALCHEMY_DATABASE_URI = "mysql+gaerdbms:///flaskr?instance=chuizonebeta:chuizonebeta"
+	migration_directory = "migrations"
