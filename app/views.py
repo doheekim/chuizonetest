60e17963f7872c72a521bae02ecd397bd25409ad
doheekim <dohee.4678@gmail.com>
2014-08-26 16:23:40 +0900

    no message

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, redirect, url_for, Flask, session
-from app import app
+from app import app, db
+# from app.forms import AcademyForm
+# from app.models import Academy
 import sys
 reload(sys)
 sys.setdefaultencoding('UTF8')
@@ -19,4 +21,40 @@ def main():
 
 @app.route('/academy')
 def mapnlist():
-	return render_template('academy.html')	
+	return render_template('academy.html')
+
+
+@app.route('/academy/create')
+def academy_create():
+	return render_template('academy_create.html')
+
+
+# @app.route('/academy/create/', methods=['GET', 'POST'])
+# def article_create():
+# 	form = AcademyForm()
+# 	if request.method == 'GET':
+# 		return render_template('academy_create.html', form=form)
+
+# 	elif request.method == 'POST':
+# 		if form.validate_on_submit():
+			
+# 			academy = Academy(
+# 				academy_name=form.academy_name.data,
+# 				teacher_name=form.teacher_name.data,
+# 				academy_introduce=form.academy_introduce.data,
+# 				teacher_introduce=form.teacher_introduce.data,
+# 				curriculum_introduce=form.curriculum_introduce.data,
+# 				academy_address=form.academy_address.data,
+# 				welcome_line=form.welcome_line.data,
+# 				phone_number=form.phone_number.data,
+# 				class_time=form.class_time.data,
+# 				class_fee=form.class_fee.data,
+# 				homepage=form.homepage.data
+# 				)
+			
+# 			db.session.add(academy)
+# 			db.session.commit()
+
+# 			# flash(u'Post Upload Success.', 'success')
+# 			return redirect(url_for('academy'))
+# 		return render_template('academy_create.html', form=form)
