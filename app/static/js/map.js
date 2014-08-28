4e173c6c8fe2572655392a1fab7bbfdcc073c0db
HogunJung <hogoonn@gmail.com>
2014-08-28 20:15:55 +0900

    no message

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 	var position101 = new daum.maps.LatLng(37.49796298370522, 127.02761094942744);
-	var position102 = new daum.maps.LatLng(37.57042061397492, 126.99213459583619);
+	var position102 = new daum.maps.LatLng(37.53589682068908, 127.13235618124992);	
+	var position123 = new daum.maps.LatLng(37.57042061397492, 126.99213459583619);	
 
 	var map = new daum.maps.Map(document.getElementById('map'), {
 		center: position102,
