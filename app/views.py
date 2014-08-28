a2be9e30f5cc73b30b137b697d43a0794fa92739
doheekim <dohee.4678@gmail.com>
2014-08-29 01:43:57 +0900

    no message

--- app/views.py
+++ app/views.py
@@ -45,26 +45,31 @@ def create():
 	if request.method == 'GET':
 		return render_template('create.html', form=form)
 	elif request.method == 'POST':
-		if form.validate_on_submit():
-			academy = Academy(
-				academy_name=form.academy_name.data,
-				teacher_name=form.teacher_name.data,
-				academy_introduce=form.academy_introduce.data,
-				teacher_introduce=form.teacher_introduce.data,
-				curriculum_introduce=form.curriculum_introduce.data,
-				academy_address=form.academy_address.data,
-				welcome_line=form.welcome_line.data,
-				phone_number=form.phone_number.data,
-				class_time=form.class_time.data,
-				class_fee=form.class_fee.data,
-				homepage=form.homepage.data,
-				location=form.location.data,
-				category=form.category.data,
-				academy_latlng=form.academy_latlng.data
-				)
+		if request.form.get('password') == 'chuizone0000':
+			if form.validate_on_submit():
+				academy = Academy(
+					academy_name=form.academy_name.data,
+					teacher_name=form.teacher_name.data,
+					academy_introduce=form.academy_introduce.data,
+					teacher_introduce=form.teacher_introduce.data,
+					curriculum_introduce=form.curriculum_introduce.data,
+					academy_address=form.academy_address.data,
+					welcome_line=form.welcome_line.data,
+					phone_number=form.phone_number.data,
+					class_time=form.class_time.data,
+					class_fee=form.class_fee.data,
+					homepage=form.homepage.data,
+					location=form.location.data,
+					category=form.category.data,
+					academy_latlng=form.academy_latlng.data
+					)
 
-			db.session.add(academy)
-			db.session.commit()
-			return redirect(url_for('academy'))
+				db.session.add(academy)
+				db.session.commit()
+				return redirect(url_for('main'))
+			return redirect(url_for('main'))
+			
+		else:
+			return redirect(url_for('main'))
 		return render_template('create.html', form=form)
 	return render_template('create.html', form=form)
