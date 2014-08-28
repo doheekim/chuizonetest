4bad0c676c46d656900a122916def82b19389777
doheekim <dohee.4678@gmail.com>
2014-08-28 19:52:47 +0900

    no message

--- app/models.py
+++ app/models.py
@@ -11,11 +11,7 @@ class Academy(db.Model):
 	teacher_introduce = db.Column(db.Text())
 	curriculum_introduce = db.Column(db.Text())
 	academy_address = db.Column(db.String(255))
-<<<<<<< HEAD
-	address_id = db.Column(db.String(255))
-=======
 	academy_latlng = db.Column(db.String(255))
->>>>>>> 93775452e2033eb5940fa7161c673e404501230e
 	welcome_line = db.Column(db.String(255))
 	phone_number = db.Column(db.String(255))
 	class_time = db.Column(db.Text())
