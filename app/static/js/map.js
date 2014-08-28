3728231617b79b3e3b18bd5e575bceba1dd45245
HogunJung <hogoonn@gmail.com>
2014-08-28 22:39:44 +0900

    no message

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,28 +1,38 @@
-window.onload = function(data) {
+$(document).ready(function(){
 	$.ajax({
 		url: '/mapnlist',
 		type: 'GET',
 		dataType: 'JSON',
+		success: function(data){
+			if(data.position){
+				var position = new daum.maps.LatLng(data.position)
+				var map = new daum.maps.Map(document.getElementById('map'), {
+					center: position,
+					level: 4,
+					mapTypeId: daum.maps.MapTypeId.ROADMAP
+				});
 
-		var position = new daum.maps.LatLng(data.position)
-		var map = new daum.maps.Map(document.getElementById('map'), {
-			center: position,
-			level: 4,
-			mapTypeId: daum.maps.MapTypeId.ROADMAP
-		});
+				var marker = new daum.maps.Marker({
+					position: position
+				});
+				marker.setMap(map);
 
-		var marker = new daum.maps.Marker({
-			position: position
-		});
-		marker.setMap(map);
-
-		var infowindow = new daum.maps.InfoWindow({
-			content: data.local + '취미학원 표시'
-		});
-		infowindow.open(map, marker);
+				var infowindow = new daum.maps.InfoWindow({
+					content: data.local + '취미학원 표시'
+				});
+				infowindow.open(map, marker);
+			}
+			else{
+				console.log('position error');
+			}
+		},
+		error: function(data) {
+			console.log('Server Error');
+		}
 	})
 });
 
 
+	
 
 
