4ee4691e01ea4ee42b1c8df4de13bbfbadb62ecd
doheekim <dohee.4678@gmail.com>
2014-08-28 22:39:40 +0900

    no message

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, Flask, session
 from app import app, db
-# from app.forms import AcademyForm
-# from app.models import Academy
+from app.forms import AcademyForm
+from app.models import Academy
 import sys
 reload(sys)
 sys.setdefaultencoding('UTF8')
@@ -15,15 +15,7 @@ def academy():
 
 @app.route('/mapnlist')
 def main():
-	s1 = request.args.get('searcher_1')
-	position = ()
-	if request.method == 'GET':
-		if s1 == '강남구':
-			position = 'position101'
-		elif s1 == '강동구':
-			position = 'position102'
-		elif s1 == '종로구':
-			position = 'position123'	
+	
     return render_template('mapnlist.html')
 
 
@@ -44,32 +36,29 @@ def search():
 		return render_template('main.html')
 
 
-# @app.route('/academy/create/', methods=['GET', 'POST'])
-# def article_create():
-# 	form = AcademyForm()
-# 	if request.method == 'GET':
-# 		return render_template('academy_create.html', form=form)
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+	form=AcademyForm()
+	if request.method == 'GET':
+		return render_template('create.html', form=form)
+	elif request.method == 'POST':
+		if form.validate_on_submit():
+			academy = Academy(
+				academy_name=form.academy_name.data,
+				teacher_name=form.teacher_name.data,
+				academy_introduce=form.academy_introduce.data,
+				teacher_introduce=form.teacher_introduce.data,
+				curriculum_introduce=form.curriculum_introduce.data,
+				academy_address=form.academy_address.data,
+				welcome_line=form.welcome_line.data,
+				phone_number=form.phone_number.data,
+				class_time=form.class_time.data,
+				class_fee=form.class_fee.data,
+				homepage=form.homepage.data
+				)
 
-# 	elif request.method == 'POST':
-# 		if form.validate_on_submit():
-			
-# 			academy = Academy(
-# 				academy_name=form.academy_name.data,
-# 				teacher_name=form.teacher_name.data,
-# 				academy_introduce=form.academy_introduce.data,
-# 				teacher_introduce=form.teacher_introduce.data,
-# 				curriculum_introduce=form.curriculum_introduce.data,
-# 				academy_address=form.academy_address.data,
-# 				welcome_line=form.welcome_line.data,
-# 				phone_number=form.phone_number.data,
-# 				class_time=form.class_time.data,
-# 				class_fee=form.class_fee.data,
-# 				homepage=form.homepage.data
-# 				)
-			
-# 			db.session.add(academy)
-# 			db.session.commit()
+			db.session.add(academy)
+			db.session.commit()
+			return redirect(url_for('mapnlist'))
 
-# 			# flash(u'Post Upload Success.', 'success')
-# 			return redirect(url_for('academy'))
-# 		return render_template('academy_create.html', form=form)
+	return render_template('create.html', form=form)
